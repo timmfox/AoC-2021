dd19cf0e36783d11896f85e784bd8a45b987e010
Tim Fox <tfox@splashfinancial.com>
2021-12-03 14:40:49 -0500

    day 3 part 2

--- src/aoc2021/day3.clj
+++ src/aoc2021/day3.clj
@@ -3,9 +3,10 @@
 
 (def sample ["00100" "11110" "10110" "10111" "10101" "01111" "00111" "11100" "10000" "11001" "00010" "01010"])
 
+#_(def input sample)
 (def input
-  (str/split-lines
-   (slurp "resources/day3.txt")))
+      (str/split-lines
+       (slurp "resources/day3.txt")))
 
 (defn str->idx-bits
   [s]
@@ -17,13 +18,12 @@
    (mapcat identity
            (map str->idx-bits coll))))
 
-;; bits are provided in left to right, not in display right to left
 (def gamma-epsilon
   (->>
    (bit-frequencies input)
    (group-by ffirst)
-   (into (sorted-map-by (comp - compare)))
-   (reduce (fn [[gamma epsilon] [_ [[[_ lb] lv] [[_ rb] rv]]]]
+   (into (sorted-map))
+   (reduce (fn [[gamma epsilon] [x [[[_ lb] lv] [[_ rb] rv]]]]
              (if (> lv rv)
                [(str gamma lb) (str epsilon rb)]
                [(str gamma rb) (str epsilon lb)]))
@@ -32,8 +32,24 @@
 (defn bits->int
   [s]
   (apply + (map-indexed
-             (fn [idx x]
-               (* (int (Math/pow 2 idx)) x))
-             (map #(Character/digit % 2) s))))
+            (fn [idx x] (* (int (Math/pow 2 idx)) x))
+            (reverse (map #(Character/digit % 2) s)))))
 
 (def part1 (apply * (map bits->int gamma-epsilon)))
+
+(defn filter-readings
+  [f ks c]
+  (loop [coll c
+         bit-idx 0]
+    (if (<= (count coll) 1)
+      (first coll)
+      (let [[[_ filter-char]] (first
+                               (sort-by second f (select-keys
+                                                  (bit-frequencies coll)
+                                                  (map (partial vector bit-idx) ks))))]
+        (recur
+         (filter #(= filter-char (get % bit-idx)) coll)
+         (inc bit-idx))))))
+
+(apply * (map bits->int [(filter-readings > [\1 \0] input)
+                         (filter-readings < [\0 \1] input)]))
